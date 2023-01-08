e9d2c168ab15e0af9c3a6f13f21027fc745fc442
prasad-kumkar <pk111_@live.com>
2023-01-08 14:42:39 +0530

    removed /abis added dynamic deployments fetching

---
+++ scripts/config.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const CONFIG = (file, env, network) => `https://raw.githubusercontent.com/synthe-x/contracts/${env}/deployments/${network}/${file}.json`;
+
+async function main() {
+    const deployments = await axios.get(CONFIG('deployments', process.env.NODE_ENV, process.env.NETWORK));
+    const config = await axios.get(CONFIG('config', process.env.NODE_ENV, process.env.NETWORK));
+    // write to ./deployments.json
+    fs.writeFileSync(process.cwd() + '/deployments/deployments.json', JSON.stringify(deployments.data, null, 2));
+    fs.writeFileSync(process.cwd() + '/deployments/config.json', JSON.stringify(config.data, null, 2));
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
